969a565d02a45d4a5672784ea243f433c0258297
Gavdzinskyi <veacheslavgavdzynsky@gmail.com>
2022-01-22 08:26:41 +0200

    Added registration and authorization, the first version

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from. import views
+from .views import *
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
 
 ]
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth import logout, login
+from main.forms import *
+from .utils import *
 
 def index(request):
     return render(request, 'main/index.html')
@@ -7,3 +12,34 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name =  'main/register.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name =  'main/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
