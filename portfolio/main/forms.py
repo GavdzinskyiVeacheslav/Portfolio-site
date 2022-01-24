b8f38626a8d4facb6bc3d1ff5739d60134855749
Gavdzinskyi <veacheslavgavdzynsky@gmail.com>
2022-01-24 22:05:00 +0200

    Added full translation of existing content en/ru - ru/en

--- portfolio/main/forms.py
+++ portfolio/main/forms.py
@@ -1,17 +1,18 @@
 from  django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label=_(u'Логин'), widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label=_(u'Пароль'), widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label=_(u'Повтор пароля'), widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label=_(u'Логин'), widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label=_(u'Пароль'), widget=forms.PasswordInput(attrs={'class': 'form-input'}))
